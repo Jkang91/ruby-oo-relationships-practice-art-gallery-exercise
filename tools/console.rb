a20f681df431309a2c55a7cc43e1d1abcb1475ec
June Kang <Jkang34@gmail.com>
2020-11-23 23:10:47 -0500

    artist labl

--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,16 @@
 require_relative '../config/environment.rb'
 
+mo = Artist.new("Mo", 10)
+to = Artist.new("To", 20)
+so = Artist.new("So", 30)
 
+ko = Gallery.new("Ko", "Chicago")
+po = Gallery.new("Po", "New York")
+lo = Gallery.new("Lo", "New Jersey")
+
+# title, price, artist, gallery
+sho = Painting.new("Sho", 1000, mo, ko)
+slo = Painting.new("Slo", 2000, to, po)
 
 
 binding.pry
